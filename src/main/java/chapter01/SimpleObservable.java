c68e2afa633ad0e68a7f053f14da7d5affc22f5b
mikten <mikael.tenhunen@avanza.se>
2017-11-09 12:57:58 +0100

    try out deprecated create

--- src/main/java/chapter01/SimpleObservable.java
+++ src/main/java/chapter01/SimpleObservable.java
@@ -1,19 +1,43 @@
 package chapter01;
 
+import java.time.LocalDateTime;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import rx.Observable;
-import rx.Observable.OnSubscribe;
 import rx.Subscriber;
-import rx.internal.operators.OnSubscribeAutoConnect;
 
 public class SimpleObservable {
 
-	public static void main(String[] args) {
-		Observable.create(new Observable.OnSubscribe<String>() {
-			public void call(Subscriber<? super String> t) {
+	private Logger log = LoggerFactory.getLogger(getClass());
+	
+	public Observable<Integer> intObserver() {
+		// Deprecated way of creating Observable with OnSubscribe
+		Observable<Integer> ints = Observable.create(new Observable.OnSubscribe<Integer>() {
+			public void call(Subscriber<? super Integer> subscriber) {
 				// TODO Auto-generated method stub
-				
+				log.info("Create");
+				subscriber.onNext(1);
+				subscriber.onNext(2);
+				subscriber.onNext(3);
+				subscriber.onCompleted();
+				log.info("completed");
 			}
 		});
+		return ints;
+	}
+	
+	public static void main(String[] args) {
+		Observable<Integer> intObserver = new SimpleObservable().intObserver();
+		intObserver.subscribe(event -> System.out.println("event1: " + event));
+		
+		Observable.create(subscriber -> {
+			subscriber.onNext(1);
+			subscriber.onNext(2);
+			subscriber.onNext(3);
+			subscriber.onCompleted();
+		}).subscribe(event -> System.out.println("event2: " + event));
 	}
 
 }
