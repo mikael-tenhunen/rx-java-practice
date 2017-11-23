a388ede6d8000bc4396276032612c5bc7d5d1133
mikten <mikael.tenhunen@avanza.se>
2017-11-23 17:06:30 +0100

    publishsubject

--- src/main/java/chapter04/pushaftercreation/PushItemsAfterCreation.java
+++ src/main/java/chapter04/pushaftercreation/PushItemsAfterCreation.java
@@ -1,5 +1,18 @@
 package chapter04.pushaftercreation;
 
+import rx.subjects.PublishSubject;
+
 public class PushItemsAfterCreation {
 	
+	public static void main(String[] args) {
+		PublishSubject<String> subject = PublishSubject.create();
+
+		
+		subject.subscribe(s -> System.out.println(s));
+		
+		subject.onNext("s1");
+		subject.onNext("s2");
+		subject.onNext("s3");
+		subject.onNext("s4");
+	}
 }
